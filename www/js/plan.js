b0daa3bb297e93fcf57ccd136aeb052b4eb6ffb0
Marcin Goryl <mcgorispl@gmail.com>
2018-01-02 20:52:07 +0100

    dupa

--- www/js/plan.js
+++ www/js/plan.js
@@ -2,9 +2,42 @@ var ilosc_list;
 var id_listy;
 
 
+function sprawdz(inputnazwa)
+{
+	return document.getElementById('inputnazwa').value;	
+}
+
+function sprawdz_czy_jest_nazwa(inputnazwa)
+
+{
+	var nazwa_listy=sprawdz(inputnazwa);
+	
+	document.getElementById('inputnazwa').value;
+	
+
+	
+	if (nazwa_listy.length < 1)
+	{
+		alert ("Podaj nazwę listy!");
+	}
+	
+	else 
+	
+	{
+		
+		alert ("nazwa jesty");
+	}
+	
+	
+	
+}
+
+
+
+
 document.getElementById('listy');
 
-
+/*
 function wypisz_listy() {
 
 for (var i=0; i<=ilosc_list)
@@ -16,3 +49,5 @@ for (var i=0; i<=ilosc_list)
 
 
 };
+
+*/
